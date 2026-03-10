31d51d5b12cfc0e6a0cae266869e20a4c286857d
unknown <jatavaditya6969@gmail.com>
2026-03-10 19:24:50 +0530

    Update WebRTC TURN Config for mobile

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -314,13 +314,10 @@ export default function ChatPage() {
             { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun1.l.google.com:19302' },
             { urls: 'stun:stun2.l.google.com:19302' },
+            { urls: 'stun:stun3.l.google.com:19302' },
+            { urls: 'stun:stun4.l.google.com:19302' },
             {
-              urls: 'turn:openrelay.metered.ca:80',
-              username: 'openrelayproject',
-              credential: 'openrelayproject',
-            },
-            {
-              urls: 'turn:openrelay.metered.ca:443',
+              urls: ['turn:openrelay.metered.ca:80', 'turn:openrelay.metered.ca:443', 'turn:openrelay.metered.ca:443?transport=tcp'],
               username: 'openrelayproject',
               credential: 'openrelayproject',
             },
